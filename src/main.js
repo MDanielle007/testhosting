2e171bb33e2f75229c750507c5c20d0bc7c05583
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2023-12-24 16:42:05 +0800

    hehe

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://tryjwtci4.test/'
+
 createApp(App).use(router).mount('#app')
